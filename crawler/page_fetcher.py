4e8e9cbe6b78faefce6e1c97350eed76f9a60c3e
rubioTorres <rubiotorres15@gmail.com>
2020-10-15 21:28:21 -0300

    init report

---
+++ crawler/page_fetcher.py
@@ -0,0 +1,57 @@
+from bs4 import BeautifulSoup
+from threading import Thread
+import requests
+import time
+import argparse
+import urllib
+from urllib.parse import urlparse, urljoin
+
+
+class PageFetcher(Thread):
+    def __init__(self, obj_scheduler):
+        super().__init__()
+        self.obj_scheduler = obj_scheduler
+
+    def request_url(self, obj_url):
+        headers = {
+            "User-Agent": "botzuck",
+        }
+        url = obj_url.geturl()
+        r = requests.get(url, headers=headers)
+
+        return r.content if "text/html" in r.headers.get("content-type") else None
+
+    def discover_links(self, obj_url, int_depth, bin_str_content):
+        """
+        Retorna os links do conteúdo bin_str_content da página já requisitada obj_url
+        """
+        soup = BeautifulSoup(bin_str_content, features="lxml")
+        for link in soup.select("a[href]"):
+            if obj_url.geturl() in link["href"] or not "http" in link["href"]:
+                url = (
+                    urlparse(link["href"])
+                    if "http" in link["href"]
+                    else urlparse(obj_url.geturl() + "/" + link["href"])
+                )
+                obj_new_url = url
+                int_new_depth = int_depth + 1
+            else:
+                obj_new_url = urlparse(link["href"])
+                int_new_depth = 0
+
+            yield obj_new_url, int_new_depth
+
+    def crawl_new_url(self):
+        url = self.obj_scheduler.get_next_url()
+        if url[0] and self.obj_scheduler.can_fetch_page(url[0]):
+            urbi = self.request_url(url[0])
+            if urbi is not None:
+                self.obj_scheduler.count_fetched_page()
+                print(url[0].geturl())
+                for url, depth in self.discover_links(url[0], url[1], urbi):
+                    self.obj_scheduler.add_new_page(url, depth)
+
+    def run(self):
+        print("Thread started!")
+        while not self.obj_scheduler.has_finished_crawl():
+            self.crawl_new_url()
--- main_test.py
+++ main_test.py
@@ -3,23 +3,15 @@ from crawler.page_fetcher import PageFetcher
 from urllib.parse import urlparse
 import ssl
 
-if __name__ == "__main__":
-    try:
-        _create_unverified_https_context = ssl._create_unverified_context
-    except AttributeError:
-        # Legacy Python that doesn't verify HTTPS certificates by default
-        pass
-    else:
-        # Handle target environment that doesn't support HTTPS verification
-        ssl._create_default_https_context = _create_unverified_https_context
+def execute_zuck(url_seeds,int_page_limit,int_depth_limit,numthread):
 
-    url_seeds = ["http://www.uol.com.br/"]
-    scheduler = Scheduler(
-        str_usr_agent="xxbot",
-        int_page_limit=30,
-        int_depth_limit=3,
-        arr_urls_seeds=url_seeds,
-    )
+    scheduler = Scheduler(str_usr_agent="zuckinhObot",
+                                int_page_limit=int_page_limit,
+                                int_depth_limit=int_depth_limit,
+                                arr_urls_seeds=url_seeds)
 
-    page_fetchers = [PageFetcher(scheduler) for _ in range(0, 5)]
-    page_fetchers[0].start()
+    page_fetchers = [PageFetcher(scheduler) for _ in range(0, numthread)]
+
+    for pg_fetcher in page_fetchers:
+        pg_fetcher.start()
+    return scheduler
